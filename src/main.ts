72acf032f8779c36479ae544360a3f0678e4f204
Rodion Galyamov <46306259+happy-mama@users.noreply.github.com>
2025-03-28 18:33:10 +0300

    custom 404 error and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./filters/http-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: process.env.USE_CORS == "true" ? true : false,
+  });
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
